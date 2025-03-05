5bcea751ede2c148a441e412131df4e283ba79f7
adam <adambd299@gmail.com>
2025-03-06 01:51:00 +0200

    setup

--- Client2/src/App.tsx
+++ Client2/src/App.tsx
@@ -6,7 +6,8 @@ import createCache from "@emotion/cache";
 import { prefixer } from "stylis";
 import rtlPlugin from "stylis-plugin-rtl";
 import AppRoutes from "./navigation/Routes";
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from "./context/AuthContext";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 // modify the theme to support RTL DO NOT REMOVE
 const rtlCache = createCache({
@@ -18,19 +19,24 @@ const rtlTheme = createTheme({
   direction: "rtl",
 });
 
+const clientId =
+  "950730786723-fieqdsnl9pdu59ulecvbve0anmeak3av.apps.googleusercontent.com";
+
 const App: React.FC = () => {
   return (
-    <AuthProvider>
-      <CacheProvider value={rtlCache}>
-        <ThemeProvider theme={rtlTheme}>
-          <div dir="rtl">
-            <BrowserRouter>
-              <AppRoutes />
-            </BrowserRouter>
-          </div>
-        </ThemeProvider>
-      </CacheProvider>
-    </AuthProvider>
+    <GoogleOAuthProvider clientId={clientId}>
+      <AuthProvider>
+        <CacheProvider value={rtlCache}>
+          <ThemeProvider theme={rtlTheme}>
+            <div dir="rtl">
+              <BrowserRouter>
+                <AppRoutes />
+              </BrowserRouter>
+            </div>
+          </ThemeProvider>
+        </CacheProvider>
+      </AuthProvider>
+    </GoogleOAuthProvider>
   );
 };
 
--- Client2/src/pages/LoginSignupScreen/api.ts
+++ Client2/src/pages/LoginSignupScreen/api.ts
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { User } from "../../types/user";
+import { CredentialResponse } from "@react-oauth/google";
 
 const API_BASE_URL = "http://localhost:3000";
 
@@ -24,6 +26,22 @@ export const loginUser = async (email: string, password: string) => {
   } catch (error: any) {
     throw error.response?.data || "Failed to login";
   }
+};
+
+export const googleSignin = async (credentialResponse: CredentialResponse) => {
+  return new Promise<User>((resolve, reject) => {
+    console.log("googleSignin...");
+    api
+      .post("/auth/google", credentialResponse)
+      .then((response: any) => {
+        console.log("googleSignin response:", response);
+        resolve(response.data);
+      })
+      .catch((error: any) => {
+        console.error("googleSignin error:", error);
+        reject(error);
+      });
+  });
 };
 
 export default api;
