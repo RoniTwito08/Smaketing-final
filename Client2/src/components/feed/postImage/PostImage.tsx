6c8994b703e70c06cccbcb0b9d6247f262f0a1d5
adam <adambd299@gmail.com>
2025-03-06 02:00:06 +0200

    fixes for setup

--- Client2/src/components/feed/postImage/PostImage.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./PostImage.css";
-
-const PostImage: React.FC<{ image: string }> = ({ image }) => {
-  return <img src={image} alt="Post" className="post-image" />;
-};
-
-export default PostImage;
