29fb09212c83c10f8556349c12aa472454fe1688
matanCohenDev <matan209225457@cs.colman.ac.il>
2025-03-29 16:12:21 +0300

    fix logo and reviewers

--- Client/src/pages/LandingPageGenerator/LandingPageSections/Header/Header.tsx
+++ Client/src/pages/LandingPageGenerator/LandingPageSections/Header/Header.tsx
@@ -1,47 +1,59 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import headerStyles from "./header.module.css";
+import { useAuth } from "../../../../context/AuthContext";
+import { businessInfoService } from "../../../../services/besinessInfo.service";
+import { config } from "../../../../config";
 
 interface HeaderProps {
   businessName: string;
-  logo: string;
   title: string;
   buttonText: string;
 }
 
-function Header({ businessName, logo, title, buttonText }: HeaderProps) {
-  const [logoPreview, setLogoPreview] = useState(logo);
+function Header({ businessName, title, buttonText }: HeaderProps) {
+  const [logoPreview, setLogoPreview] = useState<string | null>(null);
+  const { user, accessToken } = useAuth();
+  const userId = user?._id;
 
-  const handleLogoUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files && event.target.files[0]) {
-      const imageUrl = URL.createObjectURL(event.target.files[0]);
-      setLogoPreview(imageUrl);
+  useEffect(() => {
+    if (userId && accessToken && !logoPreview) {
+      businessInfoService
+        .getBusinessInfo(userId, accessToken)
+        .then((data) => {
+          if (data.data.logo) {
+            setLogoPreview(config.apiUrl + "/" + data.data.logo);
+          }
+          console.log("Business info fetched successfully:", config.apiUrl + data.logo);
+        })
+        .catch((err) => {
+          console.error("Failed to fetch business info:", err);
+        });
     }
-  };
+  }, [userId, accessToken, logoPreview]);
+
+  if (!userId || !accessToken) return null;
 
   return (
     <div className={headerStyles.headerWrapper}>
       <section className={headerStyles.headerSectionContainer}>
         <div className={headerStyles.logoContainer}>
-          <input
-            type="file"
-            accept="image/*"
-            onChange={handleLogoUpload}
-            className={headerStyles.uploadInput}
-            id="logo-upload"
-          />
-          <label htmlFor="logo-upload" className={headerStyles.uploadLabel}>
-            {logoPreview ? (
-              <img src={logoPreview} alt={title || "Section Logo"} className={headerStyles.logo} />
-            ) : (
-              <span className={headerStyles.uploadText}>בחר לוגו</span>
-            )}
-          </label>
+          {logoPreview ? (
+            <img
+              src={logoPreview}
+              alt={title || "Section Logo"}
+              className={headerStyles.logo}
+            />
+          ) : (
+            <span className={headerStyles.uploadText}>אין לוגו להציג</span>
+          )}
         </div>
         <h1 className={headerStyles.businessName}>{businessName}</h1>
         <h2 className={headerStyles.sectionTitle}>{title}</h2>
         <div className={headerStyles.headerButtonContainer}>
           <a href="#contactUs">
-            <button className={headerStyles.headerSectionButton}>{buttonText}</button>
+            <button className={headerStyles.headerSectionButton}>
+              {buttonText}
+            </button>
           </a>
         </div>
       </section>
