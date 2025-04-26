46ba25d71fb68087d996617c95db32e8970aa063
Aviv <avivush2001@cs.colman.ac.il>
2025-04-26 11:36:41 +0300

    delete post and comments

--- Client/src/pages/feedPage/MainFeed.tsx
+++ Client/src/pages/feedPage/MainFeed.tsx
@@ -1,29 +1,12 @@
-import { useFetchPosts } from "./hooks/useFetchPosts";
 import Feed from "./components/Feed";
 import "./MainFeed.css";
 
 export const MainFeed = () => {
-  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, error } =
-    useFetchPosts();
-
-  const posts = data?.pages.flatMap((page) => page.posts) || [];
-
   return (
     <div className="main-feed-container">
       <div className="main-feed">
-        {error && <p>Error loading posts</p>}
-        <Feed posts={posts} />
+        <Feed />
       </div>
-
-      {isFetchingNextPage && <div className="loader">Loading more...</div>}
-
-      {hasNextPage && !isFetchingNextPage && (
-        <div className="load-more-container">
-          <button className="load-more-btn" onClick={() => fetchNextPage()}>
-            טען עוד פוסטים
-          </button>
-        </div>
-      )}
     </div>
   );
 };
--- Client/src/pages/feedPage/components/Feed.tsx
+++ Client/src/pages/feedPage/components/Feed.tsx
@@ -1,24 +1,10 @@
-import React, { useState, useEffect } from "react";
-import { Post } from "../../../components/feed/types";
-import PostCard from "../../../components/feed/Posts/postCard/PostCard";
+import React, { useState } from "react";
 import CampaignPopup from "../../LandingPageGenerator/CampaignForm/CampaignForm"; // נתיב לקובץ שיצרנו קודם
 import "./Feed.css";
 import MyCampaigns from "../../Campaigns/MyCampaigns";
 
-const Feed: React.FC<{ posts: Post[]; className?: string }> = ({
-  posts,
-  className,
-}) => {
-  const [postList, setPostList] = useState<Post[]>(posts);
+const Feed: React.FC<{ className?: string }> = ({ className }) => {
   const [showPopup, setShowPopup] = useState(false);
-
-  useEffect(() => {
-    setPostList(posts);
-  }, [posts]);
-
-  const handleDeletePost = (postId: string) => {
-    setPostList((prevPosts) => prevPosts.filter((post) => post._id !== postId));
-  };
 
   const handleCampaignSubmit = (data: any) => {
     console.log("Campaign submitted:", data);
@@ -26,18 +12,12 @@ const Feed: React.FC<{ posts: Post[]; className?: string }> = ({
 
   return (
     <div className={`feed ${className || ""}`}>
-      {postList.length > 0 ? (
-        postList.map((post) => (
-          <PostCard key={post._id} post={post} onDelete={handleDeletePost} />
-        ))
-      ) : (
-        <div>
+      <div>
         <button className="noPostsButton" onClick={() => setShowPopup(true)}>
           צור קמפיין
         </button>
         <MyCampaigns />
-        </div>
-      )}
+      </div>
 
       <CampaignPopup
         open={showPopup}
